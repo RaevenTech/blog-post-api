78174d3a1192385a0d5e9dda1578f2373f233b29
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-09-21 14:50:54 +0100

    input form added

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+const blogPostsEl = document.getElementById("blog-posts")
+console.log(blogPostsEl)
+
 const baseURL = "https://apis.scrimba.com/jsonplaceholder/"
 
 fetch(baseURL + "posts", {method: "GET"})
   .then(resp => resp.json())
-  .then(data => console.log(data.slice(0,5)))
+  .then(data => {
+    const postsArr = data.slice(0,5)
+    let html = ""
+    postsArr.map((post) => {
+      const { title, body } = post
+      html += `
+      <h3 id="blog-title">${title}</h3>
+      <p id="blog-body">${body}</p>
+      <hr/>`
+    })
+    blogPostsEl.innerHTML = html
+  })
