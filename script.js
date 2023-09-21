f0c59241595d08a66eeeb3a3dc8789b421f14790
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-09-21 17:56:36 +0100

    POST request working
    sending new request to the server

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const blogPostsEl = document.getElementById("blog-posts")
-console.log(blogPostsEl)
 
 const baseURL = "https://apis.scrimba.com/jsonplaceholder/"
 
@@ -17,3 +16,27 @@ fetch(baseURL + "posts", {method: "GET"})
     })
     blogPostsEl.innerHTML = html
   })
+
+
+  document.getElementById("new-blog-post").addEventListener("submit", function(e){
+    e.preventDefault()
+    const postTitle = document.getElementById("blog-text-input").value
+    const postBody = document.getElementById("blog-textarea").value
+    const blogPost ={
+      title: postTitle,
+      body: postBody,
+    }
+    fetch(baseURL + "posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title: postTitle,
+        body: postBody,
+      }),
+      headers:{
+        "Content-type": "application/json"
+      }
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  })
+
