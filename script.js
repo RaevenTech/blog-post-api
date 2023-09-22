ae42469b300022076d93ced8f72b32e4fba7d143
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-09-22 16:24:50 +0100

    adding post to post list

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const blogPostsEl = document.getElementById("blog-posts")
-
 const baseURL = "https://apis.scrimba.com/jsonplaceholder/"
+
+let postsArr = []
+
+const renderPosts = () => {
+  let html = ""
+  postsArr.map((post) => {
+    const { title, body } = post
+    html += `
+    <div class="single-post">
+    <h3 id="blog-title">${title}</h3>
+    <p id="blog-body">${body}</p>
+    <hr/></div>`
+  })
+  blogPostsEl.innerHTML = html
+}
 
 fetch(baseURL + "posts", {method: "GET"})
   .then(resp => resp.json())
   .then(data => {
-    const postsArr = data.slice(0,5)
+    postsArr = data.slice(0,5)
     let html = ""
-    postsArr.map((post) => {
-      const { title, body } = post
-      html += `
-      <h3 id="blog-title">${title}</h3>
-      <p id="blog-body">${body}</p>
-      <hr/>`
-    })
-    blogPostsEl.innerHTML = html
+    renderPosts()
   })
 
 
@@ -22,21 +29,22 @@ fetch(baseURL + "posts", {method: "GET"})
     e.preventDefault()
     const postTitle = document.getElementById("blog-text-input").value
     const postBody = document.getElementById("blog-textarea").value
-    const blogPost ={
+    const blogPostData ={
       title: postTitle,
       body: postBody,
     }
-    fetch(baseURL + "posts", {
+    const options = {
       method: "POST",
-      body: JSON.stringify({
-        title: postTitle,
-        body: postBody,
-      }),
-      headers:{
-        "Content-type": "application/json"
+      body: JSON.stringify(blogPostData),
+      headers: {
+          "Content-Type": "application/json"
       }
-    })
-    .then(resp => resp.json())
-    .then(data => console.log(data))
+  }
+  
+  fetch(baseURL + "posts", options)
+      .then(res => res.json())
+      .then(postData => {
+          postsArr.unshift(postData)
+          renderPosts()
+      })
   })
-
